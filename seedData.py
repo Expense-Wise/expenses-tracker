fff804cb52dc645853accef65c6fe75246b2ec65
= <=>
2023-03-24 01:09:20 +0000

    seed data script done

--- seedData.py
+++ seedData.py
@@ -2,6 +2,7 @@ from sqlalchemy import insert
 from expenses import app, db, Expense, User
 
 with app.app_context():
+    # Define the data to be inserted into the Expense table
     expenses = [
         {
             "userId": 1,
@@ -40,15 +41,16 @@ with app.app_context():
             "category": "Travel"
         }
     ]
+    # Define the data to be inserted into the User table
     users = [
         {"email": "user1@example.com"},
         {"email": "user2@example.com"},
     ]
-
+# Construct the SQL statements for inserting the data into the Expense and User tables
     expense_stmt = insert(Expense.__table__).values(expenses)
     user_stmt = insert(User.__table__).values(users)
-
+# Execute the SQL statements using the session object
     db.session.execute(expense_stmt)
     db.session.execute(user_stmt)
-
+# Commit the changes to the database
     db.session.commit()
