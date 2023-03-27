68dd5fcd80f8d8195f4f346324e1601892d030b8
Seoras79 <seorasproductions@gmail.com>
2023-03-27 16:38:57 +0100

    created functions for total paid and unpaid

--- calcs.py
+++
@@ -1,8 +0,0 @@
-from expenses import User, db, Expense, app
-
-def allexpense(user_id):
-    user = User.query.filter_by(id=user_id).first()
-    totalexpense = 0
-    for expense in user.expenses:
-        totalexpense += expense.amount
-        return totalexpense
